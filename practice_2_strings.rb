99da50ee2991e02beac45803285f492871e2c361
“Edward <eminaya49@gmail.com>
2023-02-21 18:42:22 -0600

    completed questions 1-4 on practice 2 set problems

--- practice_2_strings.rb
+++ practice_2_strings.rb
@@ -1,12 +1,31 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+first_name = "Edward"
+last_name = "Minaya"
+puts first_name + " " + last_name
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+first_name = "Edward"
+last_name = "Minaya"
+puts "#{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+puts "Please enter a word: "
+word = gets.chomp
+if word.downcase == "marco"
+  puts "polo"
+end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+color1 = "red"
+color2 = "white"
+color3 = "purple"
+puts "My three favorite colors are " + color1 + ", " + color2 + ", and " + color3
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+color1 = "red"
+color2 = "white"
+color3 = "purple"
+puts "My three favorite colors are #{color1}, #{color2}, and #{color3}"
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
