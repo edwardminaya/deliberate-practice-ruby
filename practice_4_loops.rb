fc207c42f4c7f17169bae87fa5a6fc1c30187ced
“Edward <eminaya49@gmail.com>
2023-02-23 07:04:35 -0600

    completed first 5 exercises

--- practice_4_loops.rb
+++ practice_4_loops.rb
@@ -1,12 +1,42 @@
 # 1. Write a while loop to print the numbers 1 through 10.
+num = 1
+while num <= 10
+  puts num
+  num += 1
+end
 
 # 2. Write a while loop that prints the word "hello" 5 times.
+i = 0
+while i <= 4
+  puts "hello"
+  i += 1
+end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+while true
+  puts "Enter a word: "
+  word = gets.chomp
+  if word.downcase == "stop"
+    break
+  end
+end
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+num = 0
+while true
+  puts num
+  if num == 100
+    break
+  end
+  num += 5
+end
 
 # 5. Write a while loop that prints the number 9000 ten times.
+i = 0
+while i < 10
+  puts 9000
+  i += 1
+end
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
