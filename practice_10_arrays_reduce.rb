a5f40bbd9bf097485c20b073cc6bab769fe81733
“Edward <eminaya49@gmail.com>
2023-03-13 17:48:37 -0500

    worked on practice problem 10

--- practice_10_arrays_reduce.rb
+++ practice_10_arrays_reduce.rb
@@ -1,6 +1,15 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
 array = [5, 10, 8, 3]
+
+i = 0
+sum = 0
+while i < array.length
+  sum += array[i]
+  i += 1
+end
+p sum
+
 sum = 0
 array.each do |x|
   sum += x
@@ -10,15 +19,66 @@ p sum
 p array.reduce(:+)
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
-
 array = ["volleyball", "basketball", "badminton"]
+
+i = 0
+sum = ""
+while i < array.length
+  sum += array[i]
+  i += 1
+end
+p sum
+
+sum = ""
+array.each do |word|
+  sum += word
+end
+p sum
+
 p array.reduce("") { |x, y| x + y }
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+array = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+
+i = 0
+total_cost = 0
+while i < array.length
+  total_cost += array[i][:price]
+  i += 1
+end
+p total_cost
+
+total_cost = 0
+array.each do |price|
+  total_cost += price[:price]
+end
+p total_cost
+
+p array.reduce(0) { |sum, price| sum += price[:price].to_i }
 
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
+array = [5, 10, 8, 3, 9]
+num = array[0]
+i = 0
+while i < array.length
+  if array[i] < num
+    num = array[i]
+  end
+  i += 1
+end
+p num
+
+num = array[0]
+array.each do |x|
+  if x < num
+    num = x
+  end
+end
+p num
+
+p array.reduce(array[0]) { |num, n| num < n ? num : n }
 
 #  5. Start with an array of strings and compute the total length of all the strings.
 #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
