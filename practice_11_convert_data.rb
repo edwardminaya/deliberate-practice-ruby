37ae927f0cc78c0c8e5a66db955a75d57a3f3040
“Edward <eminaya49@gmail.com>
2023-04-03 20:29:56 -0500

    completed a few problems

--- practice_11_convert_data.rb
+++ practice_11_convert_data.rb
@@ -96,9 +96,24 @@ p array
 
 #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+name_price = { "chair" => 75, "book" => 15 }
+name_color_weight = [{ name: "chair", color: "red", weight: 10 }, { name: "book", color: "black", weight: 1 }]
+
+index = 0
+hash = Hash.new
+while index < name_price.length
+  sub_hash = Hash.new
+  sub_hash["price"] = name_price.values[index]
+  sub_hash["color"] = name_color_weight[index].values[1]
+  sub_hash["weight"] = name_color_weight[index].values[2]
+  hash[name_price.keys[index]] = sub_hash
+  index += 1
+end
+p hash
 
 #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+array = [{ author: "Jeff Smith", title: "Bone" }, { author: "George Orwell", title: "1984" }, { author: "Jeff Smith", title: "RASL" }]
 
 # 10. Given a hash, create a new hash that has the keys and values switched.
 #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
